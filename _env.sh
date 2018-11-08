801bd9b42a71c9a55d8f50fe8aa315ae0c288062
Paolo <paolo@yaoo.net>
2018-11-09 00:53:02 +0100

    consolidation ..

--- _env.sh
+++ _env.sh
@@ -119,42 +119,42 @@ phase2
     ATTEMPTS=3
     DONE=0
     while [ $DONE -eq 0 ]
-	do	
-		if [ ! -d "vlc" ]; then
-			_log "VLC source not found, cloning it .."
-			git clone $VLC_URL vlc
-			_abortIfError "can't clone VLC .."
-			cd vlc
-			[ -z "$VLC_CHECKOUT" ] || { git checkout "$VLC_CHECKOUT";	_abortIfError "can't checkout $VLC_CHECKOUT ..";	}
-		else
-			_log "VLC source found"
-			cd vlc
-		fi
+    do  
+        if [ ! -d "vlc" ]; then
+            _log "VLC source not found, cloning it .."
+            git clone $VLC_URL vlc
+            _abortIfError "can't clone VLC .."
+            cd vlc
+            [ -z "$VLC_CHECKOUT" ] || { git checkout "$VLC_CHECKOUT";   _abortIfError "can't checkout $VLC_CHECKOUT ..";    }
+        else
+            _log "VLC source found"
+            cd vlc
+        fi
         
-		if ! $(git fsck --full) ;then 
-			ATTEMPTS=$((ATTEMPTS-1))
-			_wrn "Integrity check failed! ($ATTEMPTS attempts left)"
-			cd ..
-			rm -Rf vlc
-			if [ -z $ATTEMPTS ] ; then
-				_abortError "can't get the VLC sources .."
-			fi
-		else
-			if ! $(git cat-file -e ${VLC_HASH} 2>/dev/null) ; then
-				_err "can't find expected HASH"
-				# we can eventually make this optional
-				_wrn "we don't reconize this version,\n   compile it at your own risk ..."
-				
-				if [ "$FORCE_COMMIT" = 1 ]; then
-					git reset --hard ${TESTED_HASH}
-					
-				fi
-			else
-				_log "Ok+ this is a tested commit .." 
-			fi
-			DONE=1
-		fi 
-    done			
+        if ! $(git fsck --full) ;then 
+            ATTEMPTS=$((ATTEMPTS-1))
+            _wrn "Integrity check failed! ($ATTEMPTS attempts left)"
+            cd ..
+            rm -Rf vlc
+            if [ -z $ATTEMPTS ] ; then
+                _abortError "can't get the VLC sources .."
+            fi
+        else
+            if ! $(git cat-file -e ${VLC_HASH} 2>/dev/null) ; then
+                _err "can't find expected HASH"
+                # we can eventually make this optional
+                _wrn "we don't reconize this version,\n   compile it at your own risk ..."
+                
+                if [ "$FORCE_COMMIT" = 1 ]; then
+                    git reset --hard ${TESTED_HASH}
+                    
+                fi
+            else
+                _log "Ok+ this is a tested commit .." 
+            fi
+            DONE=1
+        fi 
+    done            
     cd ..
 #-----------------------------------------------------------------------------------------------------------------------
 phase3
@@ -163,23 +163,23 @@ phase3
     _inf "bootstrapping main package .."
     NATIVE_BUILD_DIR="native-build"
     BASE_DIR="$(pwd)"
-	
+    
     cd vlc
 
     _inf "bootstrapping extra tools .."
     cd extras/tools/
     ./bootstrap
-	touch .botstrapped
+    touch .botstrapped
     _inf "building extra tools .."
     make
-	touch .built
-	cd ../../
+    touch .built
+    cd ../../
 
     [ -d "$NATIVE_BUILD_DIR" ] || mkdir "$NATIVE_BUILD_DIR"
     cd "$NATIVE_BUILD_DIR"
     ./bootstrap
-	touch .botstrapped
-	cd ../
+    touch .botstrapped
+    cd ../
     
     
     _inf "bootstrapping contrib libraries .."
@@ -187,7 +187,7 @@ phase3
     [ -d "$NATIVE_BUILD_DIR" ] || mkdir "$NATIVE_BUILD_DIR"
     cd "$NATIVE_BUILD_DIR"
     ../bootstrap
-	touch .botstrapped
+    touch .botstrapped
     # make  
     _wrn "use makeContrib <...>\n    instead\n       or change dir to $(pwd)"
     
