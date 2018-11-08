0b6b9deffed348b3d17962827a8e083623ab1cad
Paolo <paolo@yaoo.net>
2018-11-09 00:51:58 +0100

    consolidation ..

--- _env.sh
+++ _env.sh
@@ -12,8 +12,18 @@ RELEASE=0
 # override it if it make sense to you ...
 #
 VLC_URL="https://git.videolan.org/git/vlc/vlc-3.0.git"
-VLC_HASH="5a7ad1b636"
+VLC_CHECKOUT="tags/3.0.3-1"
+
+
+
+#tags/3.0.3-1
+VLC_HASH=c2bb759264
+
 FORCE_COMMIT=0
+
+
+# compiles native x86_64 but none of ARM !!
+#VLC_HASH="5a7ad1b636"
 #-----------------------------------------------------------------------------------------------------------------------
                         ESC="\033"
                         RED="$ESC[0;31m"
@@ -21,21 +31,21 @@ FORCE_COMMIT=0
                      ORANGE="$ESC[0;33m"
                      LGREEN="$ESC[1;32m"
                       LGRAY="$ESC[0;37m"
-                    LOG_COL=$GREEN
-                    INF_COL=$LGREEN
-                    WRN_COL=$ORANGE
-                    ERR_COL=$RED
+                    LOG_COL="$GREEN"
+                    INF_COL="$LGREEN"
+                    WRN_COL="$ORANGE"
+                    ERR_COL="$RED"
                     
                          NC="$ESC[0m"
 
 #-----------------------------------------------------------------------------------------------------------------------
-    _log            ()  { echo $LOG_COL- "$@" $NC ; }
+    _log            ()  { echo $LOG_COL~ "$@" $NC ; }
 #-----------------------------------------------------------------------------------------------------------------------
-    _inf            ()  { echo $INF_COL- "$@" $NC   ;   }
+    _inf            ()  { echo $INF_COL~ "$@" $NC   ;   }
 #-----------------------------------------------------------------------------------------------------------------------
-    _wrn            ()  { echo $WRN_COL- "$@" $NC   ;   }
+    _wrn            ()  { echo $WRN_COL~ "$@" $NC   ;   }
 #-----------------------------------------------------------------------------------------------------------------------
-    _err            ()  { echo $ERR_COL- "$@" $NC   ;   }
+    _err            ()  { echo $ERR_COL~ "$@" $NC   ;   }
 #-----------------------------------------------------------------------------------------------------------------------
     _abortError     ()  {
             _err "$@"
@@ -106,29 +116,45 @@ phase2
 #-----------------------------------------------------------------------------------------------------------------------
     [ 2 -ge $PROTOBUF_VER_HI ] && { _err "protobuf version < 3.x.x" : [$PROTOBUF_VER]; exit; }
 #-----------------------------------------------------------------------------------------------------------------------
-    _log "strat actual compilation .."
-
-    if [ ! -d "vlc" ]; then
-        _log "VLC source not found, cloning"
-        git clone $VLC_URL vlc
-        _abortIfError "can't clone VLC .."
-    else
-        _log "VLC source found"
-    fi
-
-    cd vlc
-
-    if ! $(git cat-file -e ${VLC_HASH} 2>/dev/null) ; then
-        _err "can't find expected HASH"
-        # we can eventually make this optional
-        _wrn "we don't reconize this version,\n   compile it at your own risk ..."
-        #exit 1
-        if [ "$FORCE_COMMIT" = 1 ]; then
-            git reset --hard ${TESTED_HASH}
-        fi
-    else
-        _log "Ok+ this is a tested commit .." 
-    fi
+    ATTEMPTS=3
+    DONE=0
+    while [ $DONE -eq 0 ]
+	do	
+		if [ ! -d "vlc" ]; then
+			_log "VLC source not found, cloning it .."
+			git clone $VLC_URL vlc
+			_abortIfError "can't clone VLC .."
+			cd vlc
+			[ -z "$VLC_CHECKOUT" ] || { git checkout "$VLC_CHECKOUT";	_abortIfError "can't checkout $VLC_CHECKOUT ..";	}
+		else
+			_log "VLC source found"
+			cd vlc
+		fi
+        
+		if ! $(git fsck --full) ;then 
+			ATTEMPTS=$((ATTEMPTS-1))
+			_wrn "Integrity check failed! ($ATTEMPTS attempts left)"
+			cd ..
+			rm -Rf vlc
+			if [ -z $ATTEMPTS ] ; then
+				_abortError "can't get the VLC sources .."
+			fi
+		else
+			if ! $(git cat-file -e ${VLC_HASH} 2>/dev/null) ; then
+				_err "can't find expected HASH"
+				# we can eventually make this optional
+				_wrn "we don't reconize this version,\n   compile it at your own risk ..."
+				
+				if [ "$FORCE_COMMIT" = 1 ]; then
+					git reset --hard ${TESTED_HASH}
+					
+				fi
+			else
+				_log "Ok+ this is a tested commit .." 
+			fi
+			DONE=1
+		fi 
+    done			
     cd ..
 #-----------------------------------------------------------------------------------------------------------------------
 phase3
@@ -137,22 +163,31 @@ phase3
     _inf "bootstrapping main package .."
     NATIVE_BUILD_DIR="native-build"
     BASE_DIR="$(pwd)"
+	
     cd vlc
+
+    _inf "bootstrapping extra tools .."
+    cd extras/tools/
+    ./bootstrap
+	touch .botstrapped
+    _inf "building extra tools .."
+    make
+	touch .built
+	cd ../../
+
     [ -d "$NATIVE_BUILD_DIR" ] || mkdir "$NATIVE_BUILD_DIR"
     cd "$NATIVE_BUILD_DIR"
-    ../bootstrap
-    
-    _inf "bootstrapping extra tools .."
-    cd ../extras/tools/
     ./bootstrap
-    _inf "making extra tools .."
-    make
+	touch .botstrapped
+	cd ../
+    
     
     _inf "bootstrapping contrib libraries .."
-    cd ../../contrib
+    cd contrib
     [ -d "$NATIVE_BUILD_DIR" ] || mkdir "$NATIVE_BUILD_DIR"
     cd "$NATIVE_BUILD_DIR"
     ../bootstrap
+	touch .botstrapped
     # make  
     _wrn "use makeContrib <...>\n    instead\n       or change dir to $(pwd)"
     
